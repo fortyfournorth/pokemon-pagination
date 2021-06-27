b2ff92465da3e87f46986bb6b9647cea359a4e8a
Christopher Vachon <cvachon@fortyfournorth.ca>
2021-06-27 14:21:17 -0400

    Update Button to allow for custom backgrounds to be added and the disabled state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,39 @@
 import { FC } from "react";
 import { ClassNames } from "@44north/classnames";
 
-const Button: FC<{ onClick: () => void; className?: string | ClassNames }> = ({
+const Button: FC<{ onClick: () => void; className?: string | ClassNames; disabled?: boolean }> = ({
     onClick = () => {},
     className = "",
-    children
+    children,
+    disabled = false,
+    ...props
 }) => {
+    const btnClassNames = new ClassNames(["border", "rounded", "py-2 px-4"]).add(className);
+
+    if (disabled) {
+        btnClassNames.add("cursor-not-allowed").add("bg-gray-700 text-gray-500");
+    }
+
+    if (!btnClassNames.has(new RegExp("^bg-"))) {
+        btnClassNames.add("bg-gray-200 hover:bg-gray-300 dark:bg-gray-500 dark:hover:bg-gray-600");
+    }
+    if (!btnClassNames.has(new RegExp("^text-"))) {
+        btnClassNames.add("text-black dark:text-white");
+    }
+
     return (
         <button
-            className={new ClassNames([
-                "border",
-                "rounded",
-                "py-2 px-4",
-                "bg-gray-200 hover:bg-gray-300 dark:bg-gray-500 dark:hover:bg-gray-600",
-                "text-black dark:text-white"
-            ])
-                .add(className)
-                .list()}
+            {...props}
+            disabled={disabled}
+            className={btnClassNames.list()}
             onClick={(event) => {
                 if (event) {
                     event.preventDefault();
                     event.stopPropagation();
+                }
+
+                if (disabled) {
+                    return;
                 }
 
                 onClick();
