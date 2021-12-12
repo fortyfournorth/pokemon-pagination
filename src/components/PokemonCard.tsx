9ecea290df8f788a19743ba5c5c379301aebb605
DaneCecchelli <danececchelli@hotmail.com>
2021-12-12 15:28:38 -0500

    Cleanup

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { ClassNames } from "@44north/classnames";
 import type { IPokemonRecord } from "./../types";
 
-const PokemonCard: FC<{ data: IPokemonRecord; info?: boolean; className?: string | ClassNames }> = ({
+const PokemonCard: FC<{ data: IPokemonRecord; className?: string | ClassNames }> = ({
     data,
     className = ""
 }) => {
