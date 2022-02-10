fabeae9fddae9ef68f918371d2ffc9c2b36e1607
DESKTOP-U5G4VFI\quang <quangpham9199@gmail.com>
2022-02-10 14:30:37 +0700

    Beutify pokemon card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -6,7 +6,23 @@ const PokemonCard: FC<{ data: IPokemonRecord; className?: string | ClassNames }>
     data,
     className = ""
 }) => {
-    return <div className={new ClassNames().add(className).list()}>{data.name}</div>;
+    return (
+        <div
+            className={new ClassNames("flex", "flex-row", "items-center", "p-2")
+                .add(className)
+                .list()}
+        >
+            <div className={new ClassNames("w-20", "h-20", "bg-cover").list()}>
+                <img
+                    src={data.sprites.official_artwork_front_default}
+                    alt={`pokemon-img-${data.id}`}
+                />
+            </div>
+            <div className={new ClassNames("p-2", "ml-2").list()}>
+                <p className={new ClassNames("font-bold", "text-xl").list()}>{data.name}</p>
+            </div>
+        </div>
+    );
 };
 
 export { PokemonCard };
